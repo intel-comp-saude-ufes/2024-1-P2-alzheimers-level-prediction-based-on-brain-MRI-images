e8dae209402ec54ae8a1c6d5285a84d81be636c6
fiorotticaio <caiofiorotti@gmail.com>
2024-06-24 21:21:59 -0300

    :bulb: adding comments

--- plots.py
+++ plots.py
@@ -5,31 +5,50 @@ from sklearn.preprocessing import label_binarize
 
 
 def plot_confusion_matrix(y_true, y_pred, class_names):
+    # Calculates the confusion matrix
     cm = confusion_matrix(y_true, y_pred)
-    cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis] * 100  # Convertendo para porcentagem
+
+    # Convert confusion matrix to percentages
+    cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis] * 100
+
+    # Create a confusion matrix display
     disp = ConfusionMatrixDisplay(confusion_matrix=cm_normalized, display_labels=class_names)
-    disp.plot(cmap=plt.cm.Blues, values_format='.2f')  # Formato para exibir porcentagem com duas casas decimais
+
+    # Plot the confusion matrix
+    disp.plot(cmap=plt.cm.Blues, values_format='.2f')
     plt.show()
 
 
 def plot_roc_curve(y_true, y_pred, class_names):
-    # Binarizando os rótulos
+    # Binarizing the labels
     y_true_binarized = label_binarize(y_true, classes=[0, 1, 2, 3])
     y_pred_binarized = label_binarize(y_pred, classes=[0, 1, 2, 3])
 
+    # Initializes dictionaries to store false positive rates (FPR), 
+    # true positive rates (TPR), and area under the curve (AUC) for each class
     fpr = {}
     tpr = {}
     roc_auc = {}
 
+    # Create a figure to plot the ROC curve
     plt.figure()
+
+    # For each class, calculate the FPR and TPR curves and the AUC
     for i, class_name in enumerate(class_names):
         fpr[class_name], tpr[class_name], _ = roc_curve(y_true_binarized[:, i], y_pred_binarized[:, i])
         roc_auc[class_name] = auc(fpr[class_name], tpr[class_name])
+
+        # Plota a curva ROC para a classe
         plt.plot(fpr[class_name], tpr[class_name], label=f'{class_name} (area = {roc_auc[class_name]:.2f})')
 
+    # Plot the ROC curve for a random classifier
     plt.plot([0, 1], [0, 1], 'k--')
+
+    # Defines the limits of the x and y axes
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
+
+    # Define labels, title and legend
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
     plt.title('Receiver Operating Characteristic')
--- train.py
+++ train.py
@@ -1,38 +1,55 @@
 import torch
 
 def train_model(device, model, train_loader, val_loader, criterion, optimizer, num_epochs=25):
+    # Iterate over the specified number of epochs
     for epoch in range(num_epochs):
+        # Set the model to training mode
         model.train()
+
+        # Initialize epoch training loss
         running_loss = 0.0
+
+        # Iterate over the training data set
         for images, labels in train_loader:
             images, labels = images.to(device), labels.to(device)
-            
-            optimizer.zero_grad()
-            outputs = model(images)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.item() * images.size(0)
+            optimizer.zero_grad() # Reset optimizer gradients
+            outputs = model(images) # Make prediction using the model
+            loss = criterion(outputs, labels) # Calculate batch loss
+            loss.backward() # Backpropagation (compute gradients)
+            optimizer.step() # Update model parameters
+            running_loss += loss.item() * images.size(0) # Accumulate loss
         
+        # Calculate epoch loss
         epoch_loss = running_loss / len(train_loader.dataset)
         print(f'Epoch {epoch+1}/{num_epochs}, Loss: {epoch_loss:.4f}')
         
         # Validation
         model.eval()
+
+        # Initialize variables to calculate loss and precision
         val_running_loss = 0.0
         correct = 0
         total = 0
+
+        # Disable gradient computation
         with torch.no_grad():
+            # Iterate over the validation data set
             for images, labels in val_loader:
                 images, labels = images.to(device), labels.to(device)
                 
-                outputs = model(images)
+                # Make prediction using the model
+                outputs = model(images) 
+
+                # Calculate batch loss
                 loss = criterion(outputs, labels)
                 val_running_loss += loss.item() * images.size(0)
+
+                # Get the class predictions
                 _, predicted = torch.max(outputs, 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
         
+        # Calculate and print validation loss and accuracy
         val_loss = val_running_loss / len(val_loader.dataset)
         val_accuracy = correct / total
         print(f'Validation Loss: {val_loss:.4f}, Accuracy: {val_accuracy:.4f}')
