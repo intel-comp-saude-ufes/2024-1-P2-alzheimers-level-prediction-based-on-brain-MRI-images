288d1ec00faf6b8d32a8b2cc1662cf74f0db8129
fiorotticaio <caiofiorotti@gmail.com>
2024-06-24 21:01:43 -0300

    :recycle: returning the labels

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ def test_model(device, model, test_loader, criterion):
     test_running_loss = 0.0
     correct = 0
     total = 0
+    all_labels = []
+    all_preds = []
     
     with torch.no_grad():
         for images, labels in test_loader:
@@ -13,10 +15,16 @@ def test_model(device, model, test_loader, criterion):
             outputs = model(images)
             loss = criterion(outputs, labels)
             test_running_loss += loss.item() * images.size(0)
+
             _, predicted = torch.max(outputs, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
+
+            all_labels.extend(labels.cpu().numpy())
+            all_preds.extend(predicted.cpu().numpy())
     
     test_loss = test_running_loss / len(test_loader.dataset)
     test_accuracy = correct / total
     print(f'\nTest Loss: {test_loss:.4f}, Accuracy: {test_accuracy:.4f}')
+
+    return all_labels, all_preds
