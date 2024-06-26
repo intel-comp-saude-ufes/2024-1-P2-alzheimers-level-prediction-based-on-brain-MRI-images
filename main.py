db360e32cc448dc8cbbc8c64f958ff428757dd5e
fiorotticaio <caiofiorotti@gmail.com>
2024-06-26 09:31:19 -0300

    :sparkles: adding cross validation in training

--- main.py
+++ main.py
@@ -1,14 +1,56 @@
+from sklearn.model_selection import StratifiedKFold
 import torch
 from torchvision import transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 import torch.nn as nn
 
 from alzheimer_dataset import AlzheimerDataset
 from simple_cnn import SimpleCNN
 from advanced_cnn import AdvancedCNN
-from train import train_model
+from train import train_model, evaluate_model
 from test import test_model
 from plots import plot_confusion_matrix, plot_roc_curve
+
+
+# Função para realizar a validação cruzada
+def cross_validate_model(device, dataset, model_class, criterion, optimizer_class, num_epochs=25, n_splits=5):
+    # Define a validação cruzada com 5 folds
+    skf = StratifiedKFold(n_splits=n_splits)
+    all_labels = []
+    all_preds = []
+    fold = 1
+    
+    # Divide os dados em treinamento e validação para cada fold
+    for train_idx, val_idx in skf.split(dataset.image_paths, dataset.labels):
+        print(f'\nFold {fold}/{n_splits}')
+        
+        # Cria os subconjuntos de treinamento e validação
+        train_subset = Subset(dataset, train_idx)
+        val_subset = Subset(dataset, val_idx)
+        
+        train_loader = DataLoader(train_subset, batch_size=32, shuffle=True)
+        val_loader = DataLoader(val_subset, batch_size=32, shuffle=False)
+        
+        # Instancia o modelo e o otimizador
+        model = model_class().to(device)
+        optimizer = optimizer_class(model.parameters(), lr=0.001)
+        
+        # Treina o modelo
+        train_model(device, model, train_loader, val_loader, criterion, optimizer, num_epochs)
+
+        # Salva o modelo após cada fold
+        torch.save(model.state_dict(), f'model_fold_{fold}.pth')
+        
+        # Avalia o modelo no conjunto de validação
+        _, _, labels, preds = evaluate_model(device, model, val_loader, criterion)
+        
+        # Armazena as etiquetas e previsões de todos os folds
+        all_labels.extend(labels)
+        all_preds.extend(preds)
+        
+        fold += 1
+    
+    return all_labels, all_preds
 
 
 if __name__ == '__main__':
@@ -24,38 +66,47 @@ if __name__ == '__main__':
         transforms.Normalize(mean=[0.5], std=[0.5])
     ])
 
+
+    print("\nTraining model...")
+
     # Loading training data
     train_dataset = AlzheimerDataset('./data/train', transform=transform)
-    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-
-    # Loading validation data
-    val_dataset = AlzheimerDataset('./data/val', transform=transform)
-    val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
-
-    # Instantiating and moving the model to the GPU
-    model = AdvancedCNN().to(device)
 
     # Defining the loss function and optimizer
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-    # Training the model
-    train_model(device, model, train_loader, val_loader, criterion, optimizer, num_epochs=1)
+    # Realiza a validação cruzada
+    all_labels, all_preds = cross_validate_model(
+        device, train_dataset, AdvancedCNN, criterion, torch.optim.Adam, num_epochs=2, n_splits=2
+    )
 
-    # Saving the trained model
-    torch.save(model.state_dict(), 'alzheimer_model.pth')
-    print("\nModel saved!")
+    # # Plotting confusion matrix
+    # class_names = ['non-demented', 'very-mild-demented', 'mild-demented', 'moderate-demented']
+    # plot_confusion_matrix(all_labels, all_preds, class_names)
 
-    # Loading the test data
+    # # Plotting ROC curve
+    # plot_roc_curve(all_labels, all_preds, class_names)
+
+
+    print("\nTesting model...")
+
+    # Carrega os dados de teste
     test_dataset = AlzheimerDataset('./data/test', transform=transform)
     test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
-    # Evaluating the model
-    all_labels, all_preds = test_model(device, model, test_loader, criterion)
+    # Instancia o modelo
+    model = AdvancedCNN().to(device)
 
-    # Plotting confusion matrix
+    # Carrega o modelo treinado
+    model.load_state_dict(torch.load('model_fold_1.pth'))
+
+    # Avalia o modelo no conjunto de teste
+    labels, preds = test_model(device, model, test_loader, criterion)
+
     class_names = ['non-demented', 'very-mild-demented', 'mild-demented', 'moderate-demented']
-    plot_confusion_matrix(all_labels, all_preds, class_names)
 
-    # Plotting ROC curve
-    plot_roc_curve(all_labels, all_preds, class_names)
+    # Plota a matriz de confusão
+    plot_confusion_matrix(labels, preds, class_names)
+
+    # Plota a curva ROC
+    plot_roc_curve(labels, preds, class_names)
--- train.py
+++ train.py
@@ -25,31 +25,31 @@ def train_model(device, model, train_loader, val_loader, criterion, optimizer, n
         
         # Validation
         model.eval()
-
-        # Initialize variables to calculate loss and precision
-        val_running_loss = 0.0
-        correct = 0
-        total = 0
-
-        # Disable gradient computation
-        with torch.no_grad():
-            # Iterate over the validation data set
-            for images, labels in val_loader:
-                images, labels = images.to(device), labels.to(device)
-                
-                # Make prediction using the model
-                outputs = model(images) 
-
-                # Calculate batch loss
-                loss = criterion(outputs, labels)
-                val_running_loss += loss.item() * images.size(0)
-
-                # Get the class predictions
-                _, predicted = torch.max(outputs, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
-        
-        # Calculate and print validation loss and accuracy
-        val_loss = val_running_loss / len(val_loader.dataset)
-        val_accuracy = correct / total
+        val_loss, val_accuracy, _, _ = evaluate_model(device, model, val_loader, criterion)
         print(f'Validation Loss: {val_loss:.4f}, Accuracy: {val_accuracy:.4f}')
+
+
+def evaluate_model(device, model, data_loader, criterion):
+    model.eval()
+    val_running_loss = 0.0
+    correct = 0
+    total = 0
+    all_labels = []
+    all_preds = []
+
+    with torch.no_grad():
+        for images, labels in data_loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            val_running_loss += loss.item() * images.size(0)
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            all_labels.extend(labels.cpu().numpy())
+            all_preds.extend(predicted.cpu().numpy())
+
+    val_loss = val_running_loss / len(data_loader.dataset)
+    accuracy = correct / total
+
+    return val_loss, accuracy, all_labels, all_preds
