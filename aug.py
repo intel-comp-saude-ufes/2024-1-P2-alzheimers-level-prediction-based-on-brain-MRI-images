cf4f32d1637686ad7d6076d5cdf924ade0e3a148
fiorotticaio <caiofiorotti@gmail.com>
2024-07-07 17:29:42 -0300

    :globe_with_meridians: translating comments

--- aug.py
+++ aug.py
@@ -6,60 +6,67 @@ import shutil
 
 def get_mri_augmentation_sequence():
     """
-    Retorna a sequência de augmentations adequada para imagens de ressonância magnética cerebral.
+    Returns the sequence of augmentations suitable for brain MRI images
 
-    Returns:
-        iaa.Sequential: Sequência de augmentations.
+    INPUT:
+        None
+
+    OUTPUT:
+        iaa.Sequential: Augmentation sequence
     """
-    # Definir a sequência de augmentações a serem aplicadas
+
+    # Define the sequence of augmentations to be applied
     seq = iaa.Sequential([
-        iaa.Fliplr(0.1),                                    # Flip horizontal com probabilidade de 10%
+        iaa.Fliplr(0.1),                                # Horizontal flip with 10% probability
         iaa.Affine(
-            rotate=(-10, 10),                               # Rotação aleatória entre -10 e 10 graus
-            shear=(-5, 5),                                  # Inclinação aleatória entre -5 e 5 graus
-            scale=(0.9, 1.1)                                # Escalamento aleatório entre 90% e 110%
+            rotate=(-10, 10),                           # Random rotation between -10 and 10 degrees
+            shear=(-5, 5),                              # Random tilt between -5 and 5 degrees
+            scale=(0.9, 1.1)                            # Random scaling between 90% and 110%
         ),
-        iaa.AdditiveGaussianNoise(scale=(0, 0.02*255)),     # Adicionar ruído gaussiano leve
-        iaa.Multiply((0.9, 1.1)),                           # Alterar o brilho
-        iaa.LinearContrast((0.9, 1.1)),                     # Alterar o contraste
+        iaa.AdditiveGaussianNoise(scale=(0, 0.02*255)), # Add Light Gaussian Noise
+        iaa.Multiply((0.9, 1.1)),                       # Change the brightness
+        iaa.LinearContrast((0.9, 1.1)),                 # Change the contrast
     ])
     return seq
 
+
 def augment_image(image):
     """
-    Realiza data augmentation em uma imagem de ressonância magnética cerebral.
+    Performs data augmentation on a brain magnetic resonance image
 
-    Args:
-        image (np.array): Imagem a ser aumentada.
+    INPUT:
+        image (np.array): Image to be augmented
 
-    Returns:
-        augmented_image (np.array): Imagem aumentada.
+    OUTPUT:
+        augmented_image (np.array): Augmented image
     """
 
     seq = get_mri_augmentation_sequence()
-
     augmented_image = seq(image=image)
-
     return augmented_image
+
 
 def augment_and_save_images(input_dir, output_dir, num_augmented_images=10):
     """
-    Aplica augmentação em imagens de ressonância magnética e salva as imagens aumentadas
-    na estrutura de diretórios correspondente.
+    Applies augmentation to MRI images and saves the augmented images in the corresponding directory structure
 
-    Args:
-        input_dir (str): Diretório contendo as imagens originais.
-        output_dir (str): Diretório onde as imagens aumentadas serão salvas.
-        num_augmented_images (int): Número de imagens aumentadas a serem geradas para cada imagem original.
+    INPUT:
+        input_dir (str): Directory containing the original images
+        output_dir (str): Directory where the augmented images will be saved
+        num_augmented_images (int): Number of augmented images to generate for each original image
+
+    OUTPUT:
+        None
     """
-    # Criar a sequência de augmentações
+
+    # Create the augmentation sequence
     augmentation_sequence = get_mri_augmentation_sequence()
 
-    # Criar a estrutura de diretórios de saída se não existir
+    # Create the output directory if it does not exist
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    # Percorrer cada subdiretório na pasta de entrada
+    # Cycle through each subdirectory in the input folder
     for category in os.listdir(input_dir):
         category_path = os.path.join(input_dir, category)
         if os.path.isdir(category_path):
@@ -67,42 +74,47 @@ def augment_and_save_images(input_dir, output_dir, num_augmented_images=10):
             if not os.path.exists(output_category_path):
                 os.makedirs(output_category_path)
 
-            # Processar cada imagem no subdiretório
+            # Process each image in the category
             for img_name in os.listdir(category_path):
                 img_path = os.path.join(category_path, img_name)
-                image = Image.open(img_path).convert('L')  # Converter para escala de cinza
+                image = Image.open(img_path).convert('L')  # Convert to grayscale
                 image_np = np.array(image)
 
-                # Gerar imagens aumentadas
+                # Generate augmented images
                 for i in range(num_augmented_images):
                     augmented_image_np = augmentation_sequence(image=image_np)
                     augmented_image = Image.fromarray(augmented_image_np)
 
-                    # Salvar a imagem aumentada
+                    # Save the augmented image
                     base_name, ext = os.path.splitext(img_name)
                     augmented_img_name = f"{base_name}_aug_{i + 1}{ext}"
                     augmented_img_path = os.path.join(output_category_path, augmented_img_name)
                     augmented_image.save(augmented_img_path)
 
+
 def balance_classes_with_augmentation(input_dir, output_dir):
     """
-    Gera augmentações para equilibrar o número de imagens em cada classe.
+    Generates augmentations to balance the number of images in each class
 
-    Args:
-        input_dir (str): Diretório contendo as imagens originais.
-        output_dir (str): Diretório onde as imagens aumentadas serão salvas.
+    INPUT:
+        input_dir (str): Directory containing the original images
+        output_dir (str): Directory where the enlarged images will be saved
+
+    OUTPUT:
+        None
     """
-    # Contar o número de imagens em cada classe
+
+    # Count the number of images in each class
     class_counts = {}
     for category in os.listdir(input_dir):
         category_path = os.path.join(input_dir, category)
         if os.path.isdir(category_path):
             class_counts[category] = len(os.listdir(category_path))
 
-    # Determinar o número máximo de imagens em qualquer classe
+    # Determine the maximum number of images in any class
     max_count = max(class_counts.values())
 
-    # Aplicar augmentação para balancear as classes
+    # Apply augmentation to balance classes
     for category in class_counts:
         category_path = os.path.join(input_dir, category)
         output_category_path = os.path.join(output_dir, category)
@@ -119,10 +131,10 @@ def balance_classes_with_augmentation(input_dir, output_dir):
 
             for img_name in existing_images:
                 img_path = os.path.join(category_path, img_name)
-                image = Image.open(img_path).convert('L')  # Converter para escala de cinza
+                image = Image.open(img_path).convert('L')  # Convert to grayscale
                 image_np = np.array(image)
 
-                # Gerar augmentações para cada imagem existente
+                # Generate augmentations for each existing image
                 for i in range(images_per_existing_image):
                     augmented_image_np = get_mri_augmentation_sequence()(image=image_np)
                     augmented_image = Image.fromarray(augmented_image_np)
@@ -131,7 +143,7 @@ def balance_classes_with_augmentation(input_dir, output_dir):
                     augmented_img_path = os.path.join(output_category_path, augmented_img_name)
                     augmented_image.save(augmented_img_path)
 
-            # Se houver imagens adicionais necessárias, gere-as a partir das primeiras imagens existentes
+            # If there are additional images required, generate them from the first existing images
             for i in range(additional_images_needed):
                 img_name = existing_images[i]
                 img_path = os.path.join(category_path, img_name)
@@ -144,10 +156,11 @@ def balance_classes_with_augmentation(input_dir, output_dir):
                 augmented_img_path = os.path.join(output_category_path, augmented_img_name)
                 augmented_image.save(augmented_img_path)
 
+
 if __name__ == '__main__':
-    # Caminhos de entrada e saída
+    # Entry and exit paths
     input_directory = "data/train"
     output_directory = "data_augmented"
 
-    # Aplicar augmentação e salvar as imagens
+    # Apply augmentation and save images
     augment_and_save_images(input_directory, output_directory, 10)
